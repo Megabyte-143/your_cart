bdbfa4a47289664d2b573b1727a5f75c6e476068
Yash Garg <yashygkf@gmail.com>
2021-09-20 17:20:08 +0530

    lint corrections

--- lib/models/dark_theme_preferences.dart
+++ lib/models/dark_theme_preferences.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class DarkThemePreferences {
   static const themeStatus = "THEMESTATUS";
 
-  void setDarkTheme(bool value) async {
+  Future<void> setDarkTheme(bool value) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
 
     await prefs.setBool(themeStatus, value);
